14dd6fa642a04047accc63e1a948cb19b47e6e66
Kuma-inataku <inataku0121@gmail,com>
2022-07-24 22:53:48 +0900

    make client

---
+++ bridge/bridge_client.php
@@ -0,0 +1,8 @@
+<?php
+
+require_once 'Listing.php';
+require_once 'ExtendedListing.php';
+require_once 'FileDataSource.php';
+
+$list1 = new Listing(new FileDataSource('data.txt'));
+$list2 = new ExtendedListing(new FileDataSource('data.txt'));

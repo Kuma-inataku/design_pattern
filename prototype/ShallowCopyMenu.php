492028a5fba039528ea131d9dd7c1f35e06c6d51
Kuma-inataku <inataku0121@gmail,com>
2022-07-24 12:20:33 +0900

    meke prototype subclass

---
+++ prototype/ShallowCopyMenu.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Prototype\Concretes;
+
+use App\Prototype\MenuPrototype;
+
+class ShallowCopyMenu extends MenuPrototype
+{
+    protected function __clone()
+    {
+        
+    }
+}

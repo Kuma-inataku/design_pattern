f047edfe6a0029c7013d76a8d48514b2c1007552
Kuma-inataku <inataku0121@gmail,com>
2022-07-24 12:20:10 +0900

    meke prototype manager

---
+++ prototype/MenuManager.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Prototype\Manager;
+
+use App\Prototype\MenuPrototype;
+use Exception;
+
+class MenuManager
+{
+    private $menus;
+
+    public function __construct()
+    {
+        $this->menus = [];
+    }
+
+    public function register(MenuPrototype $menuPrototype)
+    {
+        $this->menus[$menuPrototype->getMenuCode()] = $menuPrototype;
+    }
+
+    public function create($menu_code)
+    {
+        if (! array_key_exists($menu_code, $this->menus)) {
+            throw new Exception(sprintf('要求されたメニュー番号 %s は存在しません', $menu_code));
+        }
+
+        return $this->menus[$menu_code]->newInstance();
+    }
+}
